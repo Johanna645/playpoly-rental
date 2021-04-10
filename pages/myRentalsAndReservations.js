982fdddd84e2c4c6fd8d12c638e4547c84ad4fc0
Johanna Räihälä <johanna.raihala@gmail.com>
2021-04-10 15:05:01 +0200

    style page, link games, remove alert

--- pages/myRentalsAndReservations.js
+++ pages/myRentalsAndReservations.js
@@ -2,8 +2,11 @@ import { GetServerSidePropsContext } from 'next';
 import Layout from '../components/Layout';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Profile(props) {
+  const router = useRouter();
+
   // to prevent user from seeing other users pages
   if (!props.user) {
     return (
@@ -23,73 +26,69 @@ export default function Profile(props) {
         <title>User Profile: {props.user.username}</title>
       </Head>
 
-      <h1>Hello {props.user.username}!</h1>
-      <br />
+      <h1>My Games</h1>
+      <p>
+        Hello {props.user.username}, good to see you! If you are someone else,
+        please log out.
+      </p>
 
-      <h2>Your current rentals are</h2>
-      <div>
-        <ul>
+      <h2>My Rentals</h2>
+      <p>These games are currently with you.</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Game</th>
+          </tr>
+        </thead>
+        <tbody>
           {props.rentals.map((game) => (
-            <li key={`game-${game.name}`}>
-              {/* <Link href={`/games/${game.id}`}>
-                <a>{game.name}</a>
-              </Link> */}
-              <p>{game.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <br />
-      <h2>Your current reservations are</h2>
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Game name</th>
+            <tr key={`game-${game.name}`}>
+              <td>
+                <Link href={`/games/${game.id}`}>
+                  <a>{game.name}</a>
+                </Link>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {props.reservations.map((game) => (
-              <tr key={`game-${game.name}`}>
-                {/* <Link href={`/games/${game.id}`}>
-                <a>{game.name}</a>
-              </Link> */}
-                <td>{game.name}</td>
-                <td>
-                  <span
-                    className="btn-group"
-                    role="group"
-                    aria-label="Basic example"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-small btn-danger"
-                      value="Remove"
-                      onClick={async () => {
-                        const confirmed = window.confirm('Really remove?');
-                        if (!confirmed) return;
-                        await fetch(`/api/`, {
-                          method: 'PATCH',
-                          headers: {
-                            'Content-Type': 'application/json',
-                          },
-                          body: JSON.stringify({
-                            name: game.name,
-                            userIdReservation: null,
-                          }),
-                        });
-                      }} // diese funktioniert nicht! was für ein api-route kann ich hier eingeben?! weil für nur api(index.ts) reicht nicht diese info
-                    >
-                      Remove
-                    </button>
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>My Reservations</h2>
+      <p>You will be notified when any of these games is returned.</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Game</th>
+            <th scope="col"> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.reservations.map((game) => (
+            <tr key={`game-${game.name}`}>
+              <td>
+                <Link href={`/games/${game.id}`}>
+                  <a>{game.name}</a>
+                </Link>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-sm btn-danger"
+                  onClick={async () => {
+                    await fetch(`/api/reservation/${game.id}`, {
+                      method: 'PATCH',
+                    });
+
+                    router.push('/myRentalsAndReservations');
+                  }}
+                >
+                  Cancel Reservation
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
@@ -106,17 +105,7 @@ export async function getServerSideProps(context) {
 
   const session = await getSessionByToken(context.req.cookies.session);
 
-  // if (!session || session.userId !== Number(context.query.userId)) {
-  //   return {
-  //     props: {
-  //       user: null,
-  //       errors: [{ message: 'Access denied' }],
-  //     },
-  //   };
-  // }
-
   const user = await getUserById(session.userId);
-  // console.log('to see what user is', user);
 
   const rentals = await getAllRentalsFromUser(user.id);
 
