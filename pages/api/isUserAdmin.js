fd91f3417bc6e452c87dbb794624bb993b5511cf
Johanna Räihälä <johanna.raihala@gmail.com>
2021-04-10 10:22:01 +0200

    fix error when userId is null

--- pages/api/isUserAdmin.js
+++ pages/api/isUserAdmin.js
@@ -1,13 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { isUserAdmin, getUserIdFromSessions } from '../../util/database';
 
 export default async function handler(req, res) {
   const sessionToken = req.cookies.session;
   const userId = await getUserIdFromSessions(sessionToken);
-  const id = userId.userId;
-  // console.log(id);
 
-  const isAdmin = await isUserAdmin(id);
+  const isAdmin = userId != null && (await isUserAdmin(userId.userId));
 
   res.send({ isUserAdmin: isAdmin });
 }
