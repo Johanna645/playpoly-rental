a8102d6621ec3378338352e36625f8fbb02d1531
Johanna Räihälä <johanna.raihala@gmail.com>
2021-04-11 17:07:01 +0200

    add test data

---
+++ migrations/1618146324-createTester.js
@@ -0,0 +1,32 @@
+const argon2 = require('argon2');
+
+exports.up = async (sql) => {
+  const password = 'tester';
+  const passwordHash = await argon2.hash(password);
+
+  await sql`
+     INSERT INTO users
+       (username, password_hash)
+     VALUES
+       ('tester', ${passwordHash})
+			 `;
+
+  await sql`
+			UPDATE games
+			SET user_id_rental = 2
+			WHERE id = 1
+
+   `;
+};
+
+exports.down = async (sql) => {
+  await sql`
+    DELETE FROM users
+		WHERE id = 2
+		`;
+  await sql`
+		UPDATE games
+		SET user_id_rental = null
+		WHERE id = 1
+  `;
+};
